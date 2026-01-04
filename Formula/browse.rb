f8731babebd9ba8250d9bca572ef57689e4f05bd
PepijnSenders <ps@hellofresh.com>
2026-01-04 08:55:58 +0100

    Update browse to v1.0.4 with binary releases

--- Formula/browse.rb
+++ Formula/browse.rb
@@ -1,26 +1,45 @@
 class Browse < Formula
   desc "Scrape any webpage to markdown using your browser session"
   homepage "https://github.com/PepijnSenders/browse-cli"
-  url "https://registry.npmjs.org/browse-cli/-/browse-cli-1.0.0.tgz"
-  # SHA256 will be updated after first npm publish
-  sha256 "PLACEHOLDER_UNTIL_NPM_PUBLISH"
+  version "1.0.4"
   license "MIT"
 
-  depends_on "node"
+  on_macos do
+    on_arm do
+      url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-darwin-arm64.tar.gz"
+      sha256 "2a3f5ca48ae3fa46f6dccdfb2ed86963fb64bb4832c04601a703eb560b5c40a5"
+    end
+    on_intel do
+      url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-darwin-x64.tar.gz"
+      sha256 "816d8085cda85b1eff457893805a7f8c11f1904bc253ff0bc2462eb04668a843"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-linux-x64.tar.gz"
+      sha256 "61ae6c77066539760987dce372303cfc06b003b7512aeaaca4be219e3ad2d670"
+    end
+  end
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    # The tarball extracts to browse-{platform}/
+    # Find the binary and extension
+    bin.install Dir["browse-*/browse"].first => "browse"
+
+    # Install extension for Chrome
+    pkgshare.install Dir["browse-*/extension"].first => "extension"
+    pkgshare.install Dir["browse-*/skills"].first => "skills"
   end
 
   def caveats
     <<~EOS
-      To use browse, you need to install the Chrome extension:
+      To use browse, install the Chrome extension:
 
       1. Open chrome://extensions in Chrome
       2. Enable "Developer mode" (top right)
       3. Click "Load unpacked"
-      4. Select: #{opt_libexec}/lib/node_modules/browse-cli/extension
+      4. Select: #{pkgshare}/extension
 
       Then start the daemon:
         browse init
@@ -32,5 +51,6 @@ class Browse < Formula
 
   test do
     assert_match "browse", shell_output("#{bin}/browse --help")
+    assert_match version.to_s, shell_output("#{bin}/browse --version")
   end
 end
